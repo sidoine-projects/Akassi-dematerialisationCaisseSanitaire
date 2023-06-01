32f344c415c5083a2debad26ba09d4763495ef18
Raftfg <raftak99@gmail.com>
2023-06-01 18:41:29 +0100

    mainjs

--- src/main.js
+++ src/main.js
@@ -8,12 +8,11 @@ import VueSweetalert2 from "vue-sweetalert2";
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap/dist/js/bootstrap.js'
 
-// Vue.use(BootstrapVue);
-// Vue.use(VueSweetalert2);
+Vue.use(BootstrapVue);
+Vue.use(VueSweetalert2);
 // Vue.use(VueRecaptchaPlugin, {
 //   v2SiteKey: "6LehRTsmAAAAAHh1FjWgVrKKf9Z_OHjaiUmgeCHb",
 // });
-
 
 Vue.config.productionTip = false;
 
